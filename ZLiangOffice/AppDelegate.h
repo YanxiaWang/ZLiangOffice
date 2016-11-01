bfef103b7b6f8630fe57c6a185f1c4e2943a9396
bluemobi <bluemobi@bluemobidemac-mini-4.local>
2016-11-01 17:37:48 -0300

    Initial Commit

---
+++ ZLiangOffice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLiangOffice
+//
+//  Created by bluemobi on 16/11/1.
+//  Copyright © 2016年 中粮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZLiangOffice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLiangOffice
+//
+//  Created by bluemobi on 16/11/1.
+//  Copyright © 2016年 中粮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
